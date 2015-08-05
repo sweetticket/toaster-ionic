09278f0ca4399819597a2a2a78a40feaa3a3631e
woniesong92 <hs459@cornell.edu>
2015-08-04 20:16:07 -0700

    signup styled

--- client/templates/modals/signInSignUp/signup.js
+++ client/templates/modals/signInSignUp/signup.js
@@ -3,11 +3,11 @@ var _toggleEnableSignUp = function() {
   var password = $.trim($('#new-password').val());
   var password2 = $.trim($('#new-password2').val());
 
-  if (email !== "" && password !== "" && password2 !== "") {
-    $('.signup-btn').removeClass('disabled');
-  } else {
-    $('.signup-btn').addClass('disabled');
-  }
+  // if (email !== "" && password !== "" && password2 !== "") {
+  //   $('.signup-btn').removeClass('disabled');
+  // } else {
+  //   $('.signup-btn').addClass('disabled');
+  // }
 };
 
 Template.signUp.events({
@@ -17,22 +17,22 @@ Template.signUp.events({
 
   "click .signup-btn": function (e, template) {
     e.preventDefault();
-    var email = $('#new-email').val();
-    var password = $('#new-password').val();
-    var password2 = $('#new-password2').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var password2 = $('#password2').val();
 
-    // if (!Utils.validateEmail(email)) {
-    //   console.log("email invalid");
-    //   $('.show').removeClass('show');
-    //   $('.invalid-email').addClass('show');
-    //   return false;
+    if (!Utils.validateEmail(email)) {
+      console.log("email invalid");
+      $('.show').removeClass('show');
+      $('.invalid-email').addClass('show');
+      return false;
 
-    // } else if (password !== password2) {
-    //   $('.show').removeClass('show');
-    //   $('.pw-mismatch').addClass('show');
-    //   console.log("passwords mismatch");
-    //   return false;
-    // }
+    } else if (password !== password2) {
+      $('.show').removeClass('show');
+      $('.pw-mismatch').addClass('show');
+      console.log("passwords mismatch");
+      return false;
+    }
 
     Accounts.createUser({
       email: email,
