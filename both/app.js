bee3df5d09967ae185a8864ab059bda0bd0a92b7
woniesong92 <hs459@cornell.edu>
2015-08-04 23:04:26 -0700

    some errors fixed

--- both/app.js
+++ both/app.js
@@ -3,15 +3,6 @@
 Meteor.startup(function() {
   if (Meteor.isClient) {
     console.log("configure Push client side");
-    Push.Configure({
-      // gcm: {
-      //     // Required for Android and Chrome OS
-      //     projectNumber: 'xxxxxxxxxxxxxxxxxx'
-      // },
-      bagde: true,
-      sound: true,
-      alert: true
-    });
 
     console.log("listener bound to Push");
     Push.addListener('message', function (notification) {
