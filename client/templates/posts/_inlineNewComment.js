d4bde1b5b33456ddf5f17bd39ab07bc191d1e72d
woniesong92 <hs459@cornell.edu>
2015-08-05 18:28:19 -0700

    keyboard attached to inline comment writing footer

--- client/templates/posts/_inlineNewComment.js
+++ client/templates/posts/_inlineNewComment.js
@@ -2,6 +2,7 @@ Template._inlineNewComment.events({
   "click .inline-comment-submit": function (e, template) {
     var postId = this._id;
     var body = $(template.find(".inline-comment-body")).val();
+    $(template.find(".inline-comment-body")).val("");
     Meteor.call("Comments.new", {
       postId: postId,
       body: body
