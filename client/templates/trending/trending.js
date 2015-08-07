989f3c3bd4f18e595f2c38e41f3b0b21c2e31b61
Jenny Kim <yk494@cornell.edu>
2015-08-07 08:35:36 -0400

    trending algo, not sure if this is the best

--- client/templates/trending/trending.js
+++ client/templates/trending/trending.js
@@ -1,3 +1,23 @@
+var ranking = function(post) {
+  var now = moment();
+
+  //FIXME: REPLACE WITH DAYS BEFORE RELEASE (?). DOESNT SEEM TO WORK VERY WELL WITH
+  //POSTS THAT ARE CLOSE IN TIME (ie. within the same day --> appear in order of
+    // oldest to newest (opposite of "recent")))
+  var numHours = now.diff(post.createdAt, 'hours');
+  // console.log("ranking for ["+post.body+"]: " + post.numLikes * (1.0 / Math.pow(numDays+1,2.0)));
+  return post.numLikes * (1.0 / Math.pow(numHours+1,2.0));
+
+  }
+
+var compareRankings = function(a,b) {
+  if (ranking(a) > ranking(b))
+    return -1;
+  if (ranking(a) < ranking(b))
+    return 1;
+  return 0;
+}
+
 Template.trending.created = function () {
   this.autorun(function () {
     this.subscribe('otherUserInfo');
@@ -20,6 +40,8 @@ Template.trending.onRendered(function() {
 
 Template.trending.helpers({
   posts: function() {
-    return Posts.find({}, {sort: {numLikes: -1, createdAt: -1}});
+    var postsArr = Posts.find({}).fetch();
+    postsArr.sort(compareRankings);
+    return postsArr;
   }
 });
