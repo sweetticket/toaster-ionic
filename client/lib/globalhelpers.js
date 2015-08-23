bf5f7737f253f5a4ffea0c580c0301621a9d5a1d
Jenny Kim <yk494@cornell.edu>
2015-08-23 11:22:57 -0400

    switche to 'just now'

--- client/lib/globalhelpers.js
+++ client/lib/globalhelpers.js
@@ -1,10 +1,18 @@
 if (Meteor.isClient) {
+
+  // moment.fn.fromNowOrNow = function (a) {
+  //   if (Math.abs(moment().diff(this)) < 25000) { // 25 seconds before or after now
+  //       return 'Just now';
+  //   }
+  //   return this.fromNow(a);
+  // };
+
   Template.registerHelper("fromNow", function (createdAt) {
     moment.locale('en', {
         relativeTime : {
             future: "in %s",
             past:   "%s",
-            s:  "s",
+            s:  "Just now",
             m:  "1m",
             mm: "%dm",
             h:  "1h",
