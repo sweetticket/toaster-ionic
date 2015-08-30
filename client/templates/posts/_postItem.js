c3c99367c209820e6fc3a6ebedee28a09a85c3a5
woniesong92 <hs459@cornell.edu>
2015-08-30 03:06:50 -0400

    find a better solution than window.location

--- client/templates/posts/_postItem.js
+++ client/templates/posts/_postItem.js
@@ -3,3 +3,11 @@ Template._postItem.helpers({
     return Comments.find({postId: this._id}).count() !== 1;
   },
 });
+
+Template._postItem.events({
+  click: function (e, template) {
+    console.log("I am clicked");
+    var postId = template.data.post._id;
+    window.location = "/posts/"+postId;
+  }
+});
