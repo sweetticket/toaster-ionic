11cddd2cf6889586bdcb16fe24c502c3e277a366
Jenny Kim <yk494@cornell.edu>
2015-09-02 23:46:08 -0400

    postItem alert

--- client/templates/posts/_postItem.js
+++ client/templates/posts/_postItem.js
@@ -3,3 +3,16 @@ Template._postItem.helpers({
     return Comments.find({postId: this._id}).count() !== 1;
   },
 });
+
+Template._postItem.events({
+  "click .post-list-item": function (event, template) {
+    // console.log("isNativeApp", Utils.isNativeApp);
+    // console.log("getMobileOperatingSystem", Utils.getMobileOperatingSystem);
+    if (Utils.isNativeApp() && Utils.getMobileOperatingSystem() === 'Android') {
+      // console.log("isNativeApp && android");
+      event.preventDefault();
+      var url = $(event.currentTarget).attr('href');
+      alert(url);
+    }
+  }
+});
