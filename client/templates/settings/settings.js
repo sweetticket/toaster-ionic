41c04e9c8e245de403585fb85cdedbd7cd3ca664
Jenny Kim <yk494@cornell.edu>
2015-09-04 17:01:25 -0400

    settings alerts

--- client/templates/settings/settings.js
+++ client/templates/settings/settings.js
@@ -11,8 +11,49 @@ Template.settings.events({
       // Session.set("ready", false);
       Session.set("currentUserId", undefined);
       // window.scrollTo(0, 0);
-      Router.go('/');
+
+      if (Utils.isNativeApp() && Utils.getMobileOperatingSystem() === 'Android') {
+      // console.log("isNativeApp && android");
+        event.preventDefault();
+        alert('logout');
+      } else {
+        Router.go('/');
+      }
     });
+  },
+  'click .settings-about': function (event, template) {
+    if (Utils.isNativeApp() && Utils.getMobileOperatingSystem() === 'Android') {
+      // console.log("isNativeApp && android");
+        event.preventDefault();
+        alert('about');
+    } else {
+      Router.go('/settings/about');
+    }
+  },
+  // 'click .settings-privacy': function (event, template) {
+  //   if (Utils.isNativeApp() && Utils.getMobileOperatingSystem() === 'Android') {
+  //     // console.log("isNativeApp && android");
+  //       event.preventDefault();
+  //       alert('privacy');
+  //   } else {
+  //     Router.go('/settings/privacy');
+  //   }
+  // },
+  'click .settings-terms': function (event, template) {
+    if (Utils.isNativeApp() && Utils.getMobileOperatingSystem() === 'Android') {
+      // console.log("isNativeApp && android");
+        event.preventDefault();
+        alert('terms');
+    } else {
+      Router.go('/settings/terms');
+    }
+  },
+  'click .settings-share': function (event, template) {
+    if (Utils.isNativeApp() && Utils.getMobileOperatingSystem() === 'Android') {
+      // console.log("isNativeApp && android");
+        event.preventDefault();
+        alert('share');
+    }
   }
 });
 
